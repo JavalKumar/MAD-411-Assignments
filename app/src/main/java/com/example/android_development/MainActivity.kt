932d67eea5839a1c0706d656efc568129af8f73a
“Javalkumar <W0835521@myscc.ca>
2025-03-01 21:44:06 -0500

    Some Updates

--- app/src/main/java/com/example/android_development/MainActivity.kt
+++ app/src/main/java/com/example/android_development/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_development
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -14,18 +15,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var name: EditText
     private lateinit var show: TextView
     private lateinit var button: Button
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.editTextText)
+        show = findViewById(R.id.textView4)
+        button = findViewById(R.id.calc_button)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+        button.setOnClickListener {
+            val typedname = name.text.toString().trim()
+            show.setText("Hello "+typedname)
 
-    fun onClick(view: View){
+
+
+        }
 
     }
 
